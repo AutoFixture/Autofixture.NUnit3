Nangal/AutoFixture

Pass calculated build versions to MSBuild tasks
Some projects (e.g. NET Core) might use this information to automatically
generate the AssemblyInfo file.

Build.fsx
@@ -67,7 +67,10 @@ let build target configuration =
         | "" -> []
         | x  -> [ "AssemblyOriginatorKeyFile", FullName x ]
 
-    let properties = [ "Configuration", configuration ] @ keyFile
+    let properties = keyFile @ [ "Configuration", configuration
+                                 "AssemblyVersion", buildVersion.assemblyVersion
+                                 "FileVersion", buildVersion.fileVersion
+                                 "InformationalVersion", buildVersion.nugetVersion ]
 
     solutionsToBuild
     |> MSBuild "" target properties

AutoFixture/Autofixture.NUnit3

Build.fsx
@@ -76,7 +76,10 @@ let build target configuration =
         | "" -> []
         | x  -> [ "AssemblyOriginatorKeyFile", FullName x ]
 
-    let properties = [ "Configuration", configuration ] @ keyFile
+    let properties = keyFile @ [ "Configuration", configuration
+                                 "AssemblyVersion", buildVersion.assemblyVersion
+                                 "FileVersion", buildVersion.fileVersion
+                                 "InformationalVersion", buildVersion.infoVersion ]
 
     solutionsToBuild
     |> MSBuild "" target properties